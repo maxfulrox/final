f20444f2d0d270971f318ea6e54c26a36f98dc21
maxfulrox <44382084+maxfulrox@users.noreply.github.com>
2021-04-26 09:35:50 +0200

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,5 +1,5 @@
 # Module Imports
-import mysql.connector 
+import mysql.connector
 import sys
 import subprocess
 import time
@@ -13,6 +13,9 @@ modele = ( sys.argv[2] )
 prix = ( sys.argv[3] )
 site = ( sys.argv[4] )
 
+prix = prix.replace(',','.')
+prix = float(prix)
+
 # Connect to MariaDB Platform
 try:
     conn = mysql.connector.connect(
@@ -24,7 +27,7 @@ try:
     )
 
 except mysql.connector.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
+    print("Error connecting to MariaDB Platform")
     sys.exit(1)
 
 # Get Cursor
@@ -32,12 +35,12 @@ cur = conn.cursor()
 
 if site == "rdc":
 
-    if modele == "3070": 
+    if modele == "3070":
         prixmax = "PRIXMAX3070"
         commande = "SELECT ID, PRIXMAX3070, RDC, RTX3070, CREDIT, IDRDC, MDPRDC, IDPAYPAL, MDPPAYPAL FROM info"
-      
 
-    if modele == "3080": 
+
+    if modele == "3080":
         prixmax = "PRIXMAX3080"
         commande = "SELECT ID, PRIXMAX3080, RDC, RTX3080, CREDIT, IDRDC, MDPRDC, IDPAYPAL, MDPPAYPAL FROM info"
 
@@ -46,7 +49,7 @@ rows = cur.fetchall()
 for row in rows:
     iduser = row[0]
     prixmax = row[1]
-    prixmax = str(prixmax)
+    prixmax = float(prixmax)
     siteval = row[2]
     modeleval = row[3]
     credit = row[4]
@@ -54,9 +57,10 @@ for row in rows:
     RDCMDP = row[6]
     PAYPALMAIL = row[7]
     PAYPALMDP = row[8]
+
     if siteval ==  "1":
         if modeleval == "1":
             if prixmax >= prix:
                 if credit != "0":
-                    subprocess.Popen( ["python", "achatrdc.py", url, RDCMAIL, RDCMDP, PAYPALMAIL, PAYPALMDP, credit, iduser])
-                    time.sleep(2)
+                    subprocess.Popen( ["python", "/home/bot/final/achatrdc.py", url, RDCMAIL, RDCMDP, PAYPALMAIL, PAYPALMDP, credit, iduser])
+                    time.sleep(1)
